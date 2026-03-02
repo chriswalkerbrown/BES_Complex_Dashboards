082454d3e479df2ab9fedd975f91876d3c94aacc
chriswalkerbrown <chriswalkerbrown@gmail.com>
2026-03-02 16:53:05 +0100

    Update data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -2,17 +2,36 @@ from datetime import datetime
 from herbie import Herbie
 import xarray as xr
 
+xr.set_options(use_new_combine_kwarg_defaults=True)
+
+REQUIRED_VARS = {
+    "TMP_2maboveground",
+    "UGRD_10maboveground",
+    "VGRD_10maboveground",
+    "APCP_surface",
+    "PRATE_surface",
+}
+
 def load_caribbean(fxx=0):
     now = datetime.utcnow()
     cycle = now.replace(hour=(now.hour // 6) * 6, minute=0, second=0, microsecond=0)
 
     H = Herbie(cycle, model="nam", product="afwaca", fxx=fxx)
 
-    ds = H.xarray()
+    ds_list = H.xarray()
 
-    # Herbie returns a list of datasets when cfgrib finds multiple hypercubes
-    if isinstance(ds, list):
-        # Merge all hypercubes into one dataset
-        ds = xr.merge(ds)
+    # If Herbie returned a single dataset, just return it
+    if isinstance(ds_list, xr.Dataset):
+        return ds_list
 
-    return ds
+    # Filter hypercubes that contain at least one required variable
+    filtered = []
+    for ds in ds_list:
+        vars_in_ds = set(ds.data_vars)
+        if vars_in_ds & REQUIRED_VARS:
+            filtered.append(ds)
+
+    # Merge only the relevant hypercubes
+    merged = xr.merge(filtered, compat="override")
+
+    return merged
