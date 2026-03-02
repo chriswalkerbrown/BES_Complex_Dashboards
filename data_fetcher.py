fce7bc09383c8ce0d6d5a807852fd6dca1ecdc39
chriswalkerbrown <chriswalkerbrown@gmail.com>
2026-03-02 16:48:39 +0100

    Update data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,8 +1,18 @@
 from datetime import datetime
 from herbie import Herbie
+import xarray as xr
 
 def load_caribbean(fxx=0):
     now = datetime.utcnow()
     cycle = now.replace(hour=(now.hour // 6) * 6, minute=0, second=0, microsecond=0)
+
     H = Herbie(cycle, model="nam", product="afwaca", fxx=fxx)
-    return H.xarray()
+
+    ds = H.xarray()
+
+    # Herbie returns a list of datasets when cfgrib finds multiple hypercubes
+    if isinstance(ds, list):
+        # Merge all hypercubes into one dataset
+        ds = xr.merge(ds)
+
+    return ds
